96fee70c67d0d520d74dc161e18aee069003dc5b
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2020-10-30 12:14:58 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var date= new Date();
+
+
+
+if (date.getMonth() === 9)    {
+
+var snow=document.querySelector('.one');
+snow.className = 'two';
+
+var snow=document.querySelector('.three');
+snow.className = 'four';
+
+var snow=document.querySelector('.five');
+snow.className = 'six';
+
+var snow=document.querySelector('.seven');
+snow.className = 'eight';
+
+var snow=document.querySelector('.nine');
+snow.className = 'ten';
+
+var snow=document.querySelector('.eleven');
+snow.className = 'twelve';
+
+var snow=document.querySelector('.thirteen');
+snow.className = 'fourteen';
+
+var snow=document.querySelector('.fifteen');
+snow.className = 'sixteen';
+
+}
+
+
+else if (date.getMonth() === 0  && date.getDate() < 7) {
+
+var snow=document.querySelector('.one');
+snow.className = 'two';
+
+var snow=document.querySelector('.three');
+snow.className = 'four';
+
+var snow=document.querySelector('.five');
+snow.className = 'six';
+
+var snow=document.querySelector('.seven');
+snow.className = 'eight';
+
+var snow=document.querySelector('.nine');
+snow.className = 'ten';
+
+var snow=document.querySelector('.eleven');
+snow.className = 'twelve';
+
+var snow=document.querySelector('.thirteen');
+snow.className = 'fourteen';
+
+var snow=document.querySelector('.fifteen');
+snow.className = 'sixteen';
+}
+
+
+
+ 
+
+
